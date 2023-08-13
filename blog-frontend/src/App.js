be61fac29145f6484a5404bb78326f986965aa8a
bigtedde <big.tedde@gmail.com>
2023-08-12 21:14:25 -0700

    fixes

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+window.addEventListener('unhandledrejection', (event) => {
+    console.error('Unhandled promise rejection:', event.promise, event.reason);
+});
+
 function App() {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    fetch("https://teds-blogs-9c73db19cf47.herokuapp.com")
-      .then(res => res.json())
-      .then(data => setBlogs(data));
-  }, []);
+  fetch("https://teds-blogs-9c73db19cf47.herokuapp.com/api/blogs")
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`Server responded with status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(data => setBlogs(data))
+    .catch(error => {
+      console.error("Failed to fetch blogs:", error);
+    });
+}, []);
+
 
   return (
     <div className="App">
