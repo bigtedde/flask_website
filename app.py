e77597fadc51355f40c1d343ee662eb1a4cb60e5
bigtedde <big.tedde@gmail.com>
2023-08-13 19:30:05 -0700

    migrated to a heroku Postgres db

--- app.py
+++ app.py
@@ -4,28 +4,39 @@ Main backend for the app.
 import os
 
 from flask import Flask, jsonify, send_from_directory, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder="blog-frontend/build")
+
+# Replace postgres:// with postgresql:// for SQLAlchemy compatibility
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///local_db.db')
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+db = SQLAlchemy(app)
 
 
-blogs_list = [
-    {"id": 1, "title": "My first blog", "content": "Hello, this is my first blog!"},
-    {"id": 2, "title": "Second post", "content": "Here goes the second blog."},
-]
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
 
 
 @app.route('/api/blogs', methods=['GET', 'POST'])
 def handle_blogs():
     if request.method == 'GET':
-        return jsonify(blogs_list)
-    elif request.method == 'POST':
-        blog = request.json
-        blog['id'] = len(blogs_list) + 1
-        blogs_list.append(blog)
-        return jsonify(blog), 201
+        blogs = Blog.query.all()
+        return jsonify([{"id": blog.id, "title": blog.title, "content": blog.content} for blog in blogs])
 
+    elif request.method == 'POST':
+        blog_data = request.json
+        new_blog = Blog(title=blog_data['title'], content=blog_data['content'])
+        db.session.add(new_blog)
+        db.session.commit()
+        return jsonify({"id": new_blog.id, "title": new_blog.title, "content": new_blog.content}), 201
 
 
 @app.route("/", defaults={"path": ""})
@@ -37,5 +48,6 @@ def serve(path):
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    # npm run build
